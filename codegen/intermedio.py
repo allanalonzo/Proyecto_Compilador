fb5388ae8e7bd0f6fb812ef4a682feeaa527c4a2
Maria Mauricio <mariajosemr07@gmail.com>
2025-05-22 14:58:54 -0600

    codigo corregido

--- codegen/intermedio.py
+++ codegen/intermedio.py
@@ -4,6 +4,7 @@ class Generador3AC:
     def __init__(self):
         self.temp_count = 0
         self.codigo = []
+        self.simbolos = {}  # Tabla de símbolos para almacenar valores de variables
 
     def new_temp(self):
         temp = f"t{self.temp_count}"
@@ -18,12 +19,15 @@ class Generador3AC:
         elif isinstance(node, Declaration):
             if node.expression is not None:
                 temp = self.generar(node.expression)
+                self.simbolos[node.identifier.name] = temp
                 self.codigo.append(f"{node.identifier.name} = {temp}")
             else:
-                self.codigo.append(f"{node.identifier.name} = 0")  # Valor por defecto
+                self.simbolos[node.identifier.name] = 0
+                self.codigo.append(f"{node.identifier.name} = 0")
 
         elif isinstance(node, Assignment):
             temp = self.generar(node.expression)
+            self.simbolos[node.identifier.name] = temp
             self.codigo.append(f"{node.identifier.name} = {temp}")
 
         elif isinstance(node, BinaryOp):
@@ -38,8 +42,9 @@ class Generador3AC:
             self.codigo.append(f"{temp} = {node.value}")
             return temp
 
+
         elif isinstance(node, Identifier):
-            return node.name
+            return self.simbolos.get(node.name, node.name)  # Devuelve valor o nombre
 
     def get_code(self):
         return "\n".join(self.codigo)
